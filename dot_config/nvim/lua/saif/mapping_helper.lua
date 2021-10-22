bb987c16adc2012bc8e6387692759e95e6f7de91
isaif <saif935775@gmail.com>
2021-10-22 21:14:19 +0530

    Add: lua mapping helper file for neovim

---
+++ dot_config/nvim/lua/saif/mapping_helper.lua
@@ -0,0 +1,17 @@
+local mapping = {}
+
+local noremap = { noremap = true, silent = true }
+
+local map = function(mode, rhs, lhs, opts )
+    vim.api.nvim_set_keymap(mode, rhs, lhs, opts)
+end
+
+local nmap = function(...)
+	map('n', ...)
+end
+
+function mapping.nnoremap(lsh, rhs)
+	nmap(lsh, rhs, noremap)
+end
+
+return mapping
