a1b85dfccbbe2a33939eaba34faf6624dd2b1f70
isaif <saif935775@gmail.com>
2021-10-24 19:41:36 +0530

    Update: neovim lsp setting for lua and tsserver

--- dot_config/nvim/lua/saif/lsp/init.lua
+++ dot_config/nvim/lua/saif/lsp/init.lua
@@ -1,31 +1,51 @@
 local lsp_installer = require("nvim-lsp-installer")
-    function common_on_attach(client, bufnr)
-        -- ... set up buffer keymaps, etc.
+
+local util = require 'lspconfig/util'
+local root_pattern = util.root_pattern
+
+-- local function common_on_attach(client, bufnr)
+--   -- ... set up buffer keymaps, etc.
+-- end
+
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = common_on_attach,
+  }
+  -- (optional) Customize the options passed to the server
+  -- if server.name == "tsserver" then
+  --     opts.root_dir = function() ... end
+  -- end
+
+  if server.name == "tsserver" then
+    opts.on_attach = function(client, buffer)
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
+      common_on_attach(client, buffer)
     end
-    lsp_installer.on_server_ready(function(server)
-        local opts = {
-            on_attach = common_on_attach,
-        }
-        -- (optional) Customize the options passed to the server
-        -- if server.name == "tsserver" then
-        --     opts.root_dir = function() ... end
-        -- end
-        server:setup(opts)
-        vim.cmd [[ do User LspAttachBuffers ]]
-    end)
+  end
 
-local server = require "nvim-lsp-installer.server"
-local npm = require "nvim-lsp-installer.installers.npm"
 
-return function(name, root_dir)
-    return server.Server:new {
-        name = name,
-        root_dir = root_dir,
-        homepage = "https://github.com/typescript-language-server/typescript-language-server",
-        installer = npm.packages { "typescript-language-server", "typescript" },
-        default_options = {
-            cmd = { npm.executable(root_dir, "typescript-language-server"), "--stdio" },
+  if server.name == "sumneko_lua" then
+    opts.root_dir =  root_pattern(".git", ".editorconfig")
+    opts.settings = {
+      Lua = {
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
         },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
     }
-end
+  end
+
+  server:setup(opts)
+  vim.cmd [[ do User LspAttachBuffers ]]
+end)
 
