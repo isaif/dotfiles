772002bd3379460ad74d5b547290e4281355dd66
isaif <saif935775@gmail.com>
2021-12-24 15:05:34 +0530

    Add: format on save for null-ls

--- dot_config/nvim/lua/saif/lsp/null-ls.lua
+++ dot_config/nvim/lua/saif/lsp/null-ls.lua
@@ -31,5 +31,13 @@ nls.config {
   },
 }
 
--- require('lspconfig')["null-ls"].setup {}
-require("lspconfig")["null-ls"].setup {}
+require("lspconfig")["null-ls"].setup {
+-- you can reuse a shared lspconfig on_attach callback here
+
+-- format on save
+    on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+        end
+    end,
+}
