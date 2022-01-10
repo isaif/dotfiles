54dd3387afbc7a55305bb5ac5633c6447f0802e3
isaif <saif935775@gmail.com>
2022-01-10 20:20:19 +0530

    Remove: emmet enable for all filetype
    - by default it is enabled in all filetypes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -228,8 +228,8 @@ filetype indent on      " load indent file for specific file type
 "let g:user_emmet_mode='n'
 
 " Enable just for html/css
-let g:user_emmet_install_global = 1
-autocmd FileType html,css EmmetInstall
+" let g:user_emmet_install_global = 0
+" autocmd FileType html,css EmmetInstall
 
 
 """"""""""""""""""""""""""""""""""""
