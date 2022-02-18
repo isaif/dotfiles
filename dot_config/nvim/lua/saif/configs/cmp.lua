6afa3e646e7f2e73798b799b8f22634927339ad6
isaif <saif935775@gmail.com>
2022-02-18 14:49:56 +0530

    Add: cmp setup file

---
+++ dot_config/nvim/lua/saif/configs/cmp.lua
@@ -0,0 +1,66 @@
+local lspkind = require "lspkind"
+lspkind.init()
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lua' },
+    { name = 'nvim_lsp', max_item_count = 15 },
+    { name = 'path' },
+    { name = 'luasnip' }, -- For luasnip users.
+    { name = 'buffer', keyword_length = 5 },
+  }),
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = 'text_symbol',
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+        -- gh_issues = "[issues]",
+        -- tn = "[TabNine]",
+      },
+    },
+  },
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer', max_item_count=10 }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path', max_item_count=10 }
+  }, {
+    { name = 'cmdline', max_item_count=10 }
+  })
+})
+
+-- TODO: add nvim-cmp hisghlight groups.
+-- this will show autocomplete menu items in different colors
