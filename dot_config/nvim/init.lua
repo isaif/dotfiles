39d0d2a2ad3e59662f1f1b8d619f673f9ce578f7
isaif <saif935775@gmail.com>
2022-04-17 17:23:53 +0530

    feat: nvim better folds

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -73,6 +73,16 @@ call matchadd('ColorColumn', '\%81v', 100)
 " noremap j gj
 " noremap k gk
 
+" beautiful folds
+function FoldText()
+	let line = getline(v:foldstart)
+	let numOfLines = v:foldend - v:foldstart
+	let fillCount = winwidth('%') - len(line) - len(numOfLines) - 14
+	return line . '  ' . repeat('.', fillCount) . ' (' . numOfLines . ' L)'
+endfunction
+set foldtext=FoldText()
+set fillchars=fold:\  " removes trailing dots. Mind that there is a whitespace after the \!
+
 
 """"""""""""""""""""""""""""""""""""
 "" Sourcing init.lua """""""""""""
