8de5eba67a11b502e6b5c1eb80f1e1902003a103
isaif <saif935775@gmail.com>
2022-09-27 21:07:29 +0530

    feat(nvim): add trouble.nvim plugin
    - it shows all diagnostic errors for current project

--- dot_config/nvim/lua/saif/packer/plugins.lua
+++ dot_config/nvim/lua/saif/packer/plugins.lua
@@ -213,6 +213,18 @@ return packer.startup(function(use)
     end,
   })
 
+  use({
+    'folke/trouble.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('trouble').setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  })
+
   --##################################################################
   -- Utilities
   --##################################################################
