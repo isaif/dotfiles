92b31f66ad105c1cc4bfcce40574a624fc48d7c4
isaif <saif935775@gmail.com>
2022-10-24 14:40:06 +0530

    feat(nvim): add mapping to hide preview window in telescope

--- dot_config/nvim/lua/saif/configs/telescope.lua
+++ dot_config/nvim/lua/saif/configs/telescope.lua
@@ -11,6 +11,8 @@ require('telescope').setup({
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+
+        ['<C-p>'] = require('telescope.actions.layout').toggle_preview,
         ['<C-h>'] = 'which_key',
         ['<C-j>'] = require('telescope.actions').move_selection_next,
         ['<C-k>'] = require('telescope.actions').move_selection_previous,
