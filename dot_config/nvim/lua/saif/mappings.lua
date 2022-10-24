324be9e62814f508ea5130d8696fcfec285a7672
isaif <saif935775@gmail.com>
2022-10-24 14:35:42 +0530

    feat(nvim): use <cmd> instead of colon in mappings

--- dot_config/nvim/lua/saif/mappings.lua
+++ dot_config/nvim/lua/saif/mappings.lua
@@ -14,70 +14,70 @@ nnoremap('J', 'mzJ`z')
 --##################################################################--
 -- local nnoremap = require('saif/mapping_helper').nnoremap
 
-nnoremap('<leader>ff', ':Telescope find_files<cr>', { silent = false })
+nnoremap('<leader>ff', '<cmd>Telescope find_files<cr>', { silent = false })
 nnoremap('<leader>fg', '<cmd>Telescope live_grep<cr>')
-nnoremap('<leader>fb', ':Telescope buffers<cr>')
-nnoremap('<leader>fo', ':Telescope oldfiles<cr>')
-nnoremap('<leader>fh', ':Telescope help_tags<cr>')
-nnoremap('<leader>fc', ':Telescope commands_history<cr>')
--- nnoremap('<leader>fcc', ':Telescope commands<cr>')
-nnoremap('<leader>fq', ':Telescope quickfix<cr>')
-nnoremap('<leader>fl', ':Telescope loclist<cr>')
-nnoremap('<leader>fv', ':Telescope vim_options<cr>')
-nnoremap('<leader>fr', ':Telescope registers<cr>')
-nnoremap('<leader>fs', ':Telescope resume<cr>')
-nnoremap('<leader>fi', ':Telescope current_buffer_fuzzy_find<cr>')
-nnoremap('<leader>fe', ':Telescope diagnostics<cr>')
+nnoremap('<leader>fb', '<cmd>Telescope buffers<cr>')
+nnoremap('<leader>fo', '<cmd>Telescope oldfiles<cr>')
+nnoremap('<leader>fh', '<cmd>Telescope help_tags<cr>')
+nnoremap('<leader>fc', '<cmd>Telescope commands_history<cr>')
+-- nnoremap('<leader>fcc', '<cmd>Telescope commands<cr>')
+nnoremap('<leader>fq', '<cmd>Telescope quickfix<cr>')
+nnoremap('<leader>fl', '<cmd>Telescope loclist<cr>')
+nnoremap('<leader>fv', '<cmd>Telescope vim_options<cr>')
+nnoremap('<leader>fr', '<cmd>Telescope registers<cr>')
+nnoremap('<leader>fs', '<cmd>Telescope resume<cr>')
+nnoremap('<leader>fi', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+nnoremap('<leader>fe', '<cmd>Telescope diagnostics<cr>')
 
-nnoremap('<leader>fD', ':Telescope file_browser<cr>')
+nnoremap('<leader>fD', '<cmd>Telescope file_browser<cr>')
 nnoremap('<leader>fd', '<cmd>Telescope file_browser path=%:p:h<cr>')
 
 nnoremap(
   '<F3>',
-  [[:lua require("telescope.builtin").live_grep({search_dirs={"~/.config/nvim/lua"}})<cr>]]
+  [[<cmd>lua require("telescope.builtin").live_grep({search_dirs={"~/.config/nvim/lua"}})<cr>]]
 )
 nnoremap(
   '<F4>',
-  [[:lua require("telescope.builtin").live_grep({search_dirs={"~/vimwiki/"}})<cr>]]
+  [[<cmd>lua require("telescope.builtin").live_grep({search_dirs={"~/vimwiki/"}})<cr>]]
 )
 
 -- git related settings
-nnoremap('<leader>gcv', ':Telescope git_commits<cr>')
-nnoremap('<leader>gcc', ':Telescope git_bcommits<cr>')
+nnoremap('<leader>gcv', '<cmd>Telescope git_commits<cr>')
+nnoremap('<leader>gcc', '<cmd>Telescope git_bcommits<cr>')
 
 --##################################################################--
 -- Harpoon
 --##################################################################--
 
 -- add file to list
-nnoremap(',G', ':lua require("harpoon.mark").add_file()<CR>')
+nnoremap(',G', '<cmd>lua require("harpoon.mark").add_file()<CR>')
 
 -- add file at particular index
-nnoremap(',ga', ':lua require("harpoon.mark").set_current_at(1)<CR>')
-nnoremap(',gs', ':lua require("harpoon.mark").set_current_at(2)<CR>')
-nnoremap(',gd', ':lua require("harpoon.mark").set_current_at(3)<CR>')
-nnoremap(',gf', ':lua require("harpoon.mark").set_current_at(4)<CR>')
+nnoremap(',ga', '<cmd>lua require("harpoon.mark").set_current_at(1)<CR>')
+nnoremap(',gs', '<cmd>lua require("harpoon.mark").set_current_at(2)<CR>')
+nnoremap(',gd', '<cmd>lua require("harpoon.mark").set_current_at(3)<CR>')
+nnoremap(',gf', '<cmd>lua require("harpoon.mark").set_current_at(4)<CR>')
 
 nnoremap(
   ',gg',
-  ':lua require("harpoon.ui").toggle_quick_menu()<CR>',
+  '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>',
   { silent = false }
 )
 
-nnoremap(',a', ':lua require("harpoon.ui").nav_file(1)<CR>')
-nnoremap(',s', ':lua require("harpoon.ui").nav_file(2)<CR>')
-nnoremap(',d', ':lua require("harpoon.ui").nav_file(3)<CR>')
-nnoremap(',f', ':lua require("harpoon.ui").nav_file(4)<CR>')
--- nnoremap(',f', ':lua require("harpoon.ui").nav_file(4)')
+nnoremap(',a', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>')
+nnoremap(',s', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>')
+nnoremap(',d', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>')
+nnoremap(',f', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>')
+-- nnoremap(',f', '<cmd>lua require("harpoon.ui").nav_file(4)')
 
 --##################################################################--
 -- Fugigitive
 --##################################################################--
 
-nnoremap('<leader>gg', ':tab G<CR>')
-nnoremap('<leader>gs', ':Git<CR>')
-nnoremap('<leader>gd', ':Gdiffsplit<CR>')
-nnoremap('<leader>gb', ':Git blame<CR>')
+nnoremap('<leader>gg', '<cmd>tab G<CR>')
+nnoremap('<leader>gs', '<cmd>Git<CR>')
+nnoremap('<leader>gd', '<cmd>Gdiffsplit<CR>')
+nnoremap('<leader>gb', '<cmd>Git blame<CR>')
 --nnoremap <leader>ga :Git add %:p<CR><CR>
 --nnoremap <leader>gs :Git<CR>
 --nnoremap <leader>gc :Gcommit -v -q<CR>
@@ -102,12 +102,12 @@ nnoremap('<leader>gb', ':Git blame<CR>')
 xnoremap('il', '<ESC>^vg_')
 xnoremap('al', '<ESC>0v$')
 --  operator pending mode
-onoremap('il', ':<C-u>normal! ^vg_<CR>')
-onoremap('al', ':<C-u>normal! 0v$<CR>')
+onoremap('il', '<cmd><C-u>normal! ^vg_<CR>')
+onoremap('al', '<cmd><C-u>normal! 0v$<CR>')
 
 -- Complete buffer
 -- xnoremap(', '<ESC>myggVG`y')
-onoremap('A', ':<C-u>normal! ggVG<CR>')
+onoremap('A', '<cmd><C-u>normal! ggVG<CR>')
 
 --##################################################################--
 -- Navigation
@@ -120,8 +120,8 @@ nnoremap('g3', '3gt')
 nnoremap('g4', '4gt')
 
 -- navigate to previous and next buffers
-nnoremap('[b', ':bprevious<CR>')
-nnoremap(']b', ':bnext<CR>')
+nnoremap('[b', '<cmd>bprevious<CR>')
+nnoremap(']b', '<cmd>bnext<CR>')
 
 -- Navigate windows using Ctrl
 nnoremap('<C-h>', '<C-w>h')
@@ -160,28 +160,28 @@ inoremap('jj', '<ESC>')
 tnoremap('jj', '<C-\\><C-N>')
 
 -- disable highlight on esc
-nnoremap('<ESC>', ':nohls<CR><ESC>')
+nnoremap('<ESC>', '<cmd>nohls<CR><ESC>')
 
 -- open file manager
-nnoremap('<F2>', ':Lex<CR>')
+nnoremap('<F2>', '<cmd>Lex<CR>')
 
 -- Save all file
-nnoremap('<Leader>a', ':wa<CR>')
+nnoremap('<Leader>a', '<cmd>wa<CR>')
 
 -- Uppercase a word
 nnoremap(',u', 'vaWUe')
 
 -- Replace a WORD under cursor in file and line
 -- this doesn't work correctly as it searchs for WORD followed by a space
-nnoremap(',rrf', ':%s/<C-r><C-a> //g<Left><Left>')
-nnoremap(',rrl', ':s/<C-r><C-a> //g<Left><Left>')
+nnoremap(',rrf', '<cmd>%s/<C-r><C-a> //g<Left><Left>')
+nnoremap(',rrl', '<cmd>s/<C-r><C-a> //g<Left><Left>')
 
 -- Replace a word under cursor in file and line
 -- a word can contain alphbets numerals underscore
 -- \<\> is use to match exact
 -- :%s/\<WordToReplace\>/New/g
-nnoremap(',rf', [[:%s/\<<C-r><C-w>\>//g<Left><Left>]])
-nnoremap(',rl', [[:s/\<<C-r><C-w>\>//g<Left><Left>]])
+nnoremap(',rf', [[<cmd>%s/\<<C-r><C-w>\>//g<Left><Left>]])
+nnoremap(',rl', [[<cmd>s/\<<C-r><C-w>\>//g<Left><Left>]])
 
 -- vimp.nnoremap(',vr', function()
 --   vimp.unmap_all()
@@ -237,4 +237,4 @@ nnoremap('<Leader>zo', '<c-w>=')
 -- vim.opt.complete += kspell
 
 -- toggle spell on
-nnoremap(',ts', ':set spell!<CR>')
+nnoremap(',ts', '<cmd>set spell!<CR>')
