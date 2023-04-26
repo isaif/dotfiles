6c4808a6caf5b8fe85480d5c7c4f07ed0bf7ad0f
isaif <saif935775@gmail.com>
2023-04-26 14:13:43 +0530

    feat(nvim): enable wrap option

--- dot_config/LazyNvim/lua/config/options.lua
+++ dot_config/LazyNvim/lua/config/options.lua
@@ -105,7 +105,7 @@ opt.undolevels = 10000
 -- opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = 'longest:full,full' -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+-- opt.wrap = false -- Disable line wrap
 opt.updatetime = 100 -- faster completion (4000ms default)
 
 opt.splitkeep = 'screen'
