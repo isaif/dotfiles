77f42cf07caf444e1c9876eb9caf8a4de9fb053f
isaif <saif935775@gmail.com>
2023-05-08 18:38:53 +0530

    feat(awesomewm): modify default layout to tile left

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -63,8 +63,8 @@ local modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile.left,
     awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
