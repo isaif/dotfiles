6e74269582bdbb407d20afca8df8435dc3f02636
isaif <saif935775@gmail.com>
2023-05-09 18:09:48 +0530

    feat(awesomewm): remove menu that has launcher

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -88,40 +88,6 @@ awful.layout.layouts = {
 }
 -- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
-local myawesomemenu = {
-  {
-    'hotkeys',
-    function()
-      hotkeys_popup.show_help(nil, awful.screen.focused())
-    end,
-  },
-  { 'manual', terminal .. ' -e man awesome' },
-  { 'edit config', editor_cmd .. ' ' .. awesome.conffile },
-  { 'restart', awesome.restart },
-  {
-    'quit',
-    function()
-      awesome.quit()
-    end,
-  },
-}
-
-local mymainmenu = awful.menu({
-  items = {
-    { 'awesome', myawesomemenu, beautiful.awesome_icon },
-    { 'open terminal', terminal },
-  },
-})
-
--- mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
--- menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
 -- Keyboard map indicator and switcher
 local mykeyboardlayout = awful.widget.keyboardlayout()
 
@@ -256,15 +222,13 @@ end)
 -- }}}
 
 -- {{{ Mouse bindings
-root.buttons(
-  gears.table.join(
-    awful.button({}, 3, function()
-      mymainmenu:toggle()
-    end),
-    awful.button({}, 4, awful.tag.viewnext),
-    awful.button({}, 5, awful.tag.viewprev)
-  )
-)
+root.buttons(gears.table.join(
+  -- awful.button({}, 3, function()
+  --   mymainmenu:toggle()
+  -- end),
+  awful.button({}, 4, awful.tag.viewnext),
+  awful.button({}, 5, awful.tag.viewprev)
+))
 -- }}}
 
 local confirmQuitmenu = awful.menu({
