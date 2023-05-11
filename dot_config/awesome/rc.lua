227438950a22b336ed44a384e1d97e81dc98632e
isaif <saif935775@gmail.com>
2023-05-11 18:50:23 +0530

    refactor(awesomewm): remove client.connect_signal(manage)
    - awesomewm-git version config doesn't have this

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -164,25 +164,6 @@ awful.rules.rules = {
 }
 -- }}}
 
--- {{{ Signals
--- Signal function to execute when a new client appears.
-client.connect_signal('manage', function(c)
-  -- Set the windows at the slave,
-  -- i.e. put it at the end of others instead of setting it master.
-  if not awesome.startup then
-    awful.client.setslave(c)
-  end
-
-  if
-    awesome.startup
-    and not c.size_hints.user_position
-    and not c.size_hints.program_position
-  then
-    -- Prevent clients from being unreachable after screen count changes.
-    awful.placement.no_offscreen(c)
-  end
-end)
-
 -- {{{ Titlebars
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal('request::titlebars', function(c)
@@ -227,4 +208,3 @@ end)
 client.connect_signal('mouse::enter', function(c)
   c:activate({ context = 'mouse_enter', raise = false })
 end)
--- }}}
