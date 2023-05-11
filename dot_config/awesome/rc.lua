d12199eb5b95644b624116c8f1aee613d6c9919b
isaif <saif935775@gmail.com>
2023-05-11 19:29:32 +0530

    fix(awesome): layout setting not working
    - it should be in correct order after the variable settings

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -28,6 +28,30 @@ end)
 
 require('my_variables').set_theme()
 
+-- -- Them-- {{{ Tag layout
+-- Table of layouts to cover with awful.layout.inc, order matters.
+tag.connect_signal('request::default_layouts', function()
+  awful.layout.append_default_layouts({
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.max,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.corner.nw,
+    -- awful.layout.suit.floating,
+    -- awful.layout.suit.corner.ne,
+    -- awful.layout.suit.corner.sw,
+    -- awful.layout.suit.corner.se,
+  })
+end)
+-- }}}
+
 -- {{{ Wallpaper
 screen.connect_signal('request::wallpaper', function(s)
   awful.wallpaper({
@@ -55,30 +79,6 @@ screen.connect_signal('request::desktop_decoration', function(s)
   my_bar(s)
 end)
 --}}}
-
--- {{{ Tag layout
--- Table of layouts to cover with awful.layout.inc, order matters.
-tag.connect_signal('request::default_layouts', function()
-  awful.layout.append_default_layouts({
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.floating,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-  })
-end)
--- }}}
 
 -- {{{ Store the list of mouse buttons to be applied on the wallpaper
 -- (also known as root window).
