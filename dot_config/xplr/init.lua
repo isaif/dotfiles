f9bf06facb38f481d2abfdfc83a4f2cb6eb3ca22
isaif <saif935775@gmail.com>
2023-05-15 17:14:29 +0530

    feat(xplr): add icons plugin to show icons for files

--- dot_config/xplr/init.lua
+++ dot_config/xplr/init.lua
@@ -1,1 +1,10 @@
 version = '0.21.1'
+
+local home = os.getenv('HOME')
+package.path = home
+  .. '/.config/xplr/plugins/?/init.lua;'
+  .. home
+  .. '/.config/xplr/plugins/?.lua;'
+  .. package.path
+
+require('icons').setup()
