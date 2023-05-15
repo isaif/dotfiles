f71ed6f9760977d61a543bbc64509b1ae75e8f50
isaif <saif935775@gmail.com>
2023-05-15 17:15:00 +0530

    feat(xplr): add tri-pane plugin to show panes like ranger

--- dot_config/xplr/init.lua
+++ dot_config/xplr/init.lua
@@ -8,3 +8,17 @@ package.path = home
   .. package.path
 
 require('icons').setup()
+
+require('tri-pane').setup()
+
+-- If you set as_default_layout = false,
+-- switch to this mode manually by pressing ctrl-w and then T.
+-- require('tri-pane').setup({
+--   layout_key = 'T', -- In switch_layout mode
+--   as_default_layout = true,
+--   left_pane_width = { Percentage = 20 },
+--   middle_pane_width = { Percentage = 50 },
+--   right_pane_width = { Percentage = 30 },
+--   left_pane_renderer = custom_function_to_render_left_pane,
+--   right_pane_renderer = custom_function_to_render_right_pane,
+-- })
