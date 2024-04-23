1810c2a5074aecc91b31017a4543457b267d9d6d
isaif <saif935775@gmail.com>
2024-04-23 21:44:59 +0530

    feat(nvim): add undotree plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -17,4 +17,12 @@ return {
 
   -- provides highlights for kmonad configuration file
   { 'kmonad/kmonad-vim', ft = 'kbd' },
+  {
+    'jiaoshijie/undotree',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
 }
