8f0f39c0bad96b239780fdedaacd16277102cf7a
isaif <saif935775@gmail.com>
2024-05-22 22:09:59 +0530

    feat(nvim): add arena plugin
    - Quick buffer navigation
    - it shows the 5 most used buffer

---
+++ dot_config/nvim/lua/plugins/arena.lua
@@ -0,0 +1,40 @@
+local function navigate(num)
+  vim.fn.cursor(num, 0)
+
+  -- Simulate pressing the Enter key
+  vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes('<CR>', true, false, true),
+    'm',
+    false
+  )
+end
+
+return {
+  'dzfrias/arena.nvim',
+  event = 'BufWinEnter',
+  keys = { { '<leader>b', '<cmd>ArenaToggle<CR>' } },
+  -- Calls `.setup()` automatically
+  config = {
+    buf_opts = {
+      ['relativenumber'] = false,
+    },
+    -- In setup options
+    keybinds = {
+      ['1'] = function()
+        navigate(1)
+      end,
+      ['2'] = function()
+        navigate(2)
+      end,
+      ['3'] = function()
+        navigate(3)
+      end,
+      ['4'] = function()
+        navigate(4)
+      end,
+      ['5'] = function()
+        navigate(5)
+      end,
+    },
+  },
+}
