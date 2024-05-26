031b9760d62985d8c5f35f6471a592c53af69a27
isaif <saif935775@gmail.com>
2024-05-26 22:10:30 +0530

    feat(nvim): add registers plugin
    - good for copy and paste

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -26,6 +26,16 @@ return {
     },
   },
   {
+  {
+    'tversteeg/registers.nvim',
+    cmd = 'Registers',
+    config = true,
+    keys = {
+      { '"', mode = { 'n', 'v' } },
+      { '<C-R>', mode = 'i' },
+    },
+    name = 'registers',
+  },
     'declancm/maximize.nvim',
     keys = {
       {
