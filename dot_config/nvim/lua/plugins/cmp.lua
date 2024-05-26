16adec95e07225519d587e7d66f2e0b38e43ff5d
isaif <saif935775@gmail.com>
2024-05-26 22:19:40 +0530

    feat(nvim): add cmp based plugins to improve usability

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -5,16 +5,38 @@ return {
 
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
+    -- 'hrsh7th/cmp-buffer',
+    -- 'hrsh7th/cmp-path',
+
     'hrsh7th/cmp-nvim-lua',
     -- 'saadparwaiz1/cmp_luasnip',
+
+    { 'hrsh7th/cmp-cmdline', event = { 'CmdlineEnter' } },
+
+    -- completion source for all file contents in a directory using ripgrep
+    -- using this in insert mode completion not for searching with / or ?
+    'lukas-reineke/cmp-rg',
+
+    -- fuzzy for buffer contents
+    -- rg is good but it can't handle spaces between words
+    -- can't use rg for / and ? as it would search whole project
+    -- cmp-buffer is also fuzzy but
+    { 'tzachar/cmp-fuzzy-buffer', dependencies = { 'tzachar/fuzzy.nvim' } },
+
+    -- fuzzy for path and buffer contents
+    { 'tzachar/cmp-fuzzy-path', dependencies = { 'tzachar/fuzzy.nvim' } },
+
+    -- 'kbwo/cmp-yank',
+
+    -- provides a source for all the lines in the current buffer.
+    -- This is especially useful for C programmers.
+    -- It uses tree-sitter (optional) if you have it installed on your system.
+    -- it can take space between words, it matched three partial words with space
+    -- it can also be used for / and ? but cmp-fuzzy-buffer is providing same feature
+    'amarakon/nvim-cmp-buffer-lines',
+
     -- lspkind to show symbols in completion menu
     'onsails/lspkind-nvim',
-    { 'hrsh7th/cmp-cmdline', event = { 'CmdlineEnter' } },
-    -- {
-    --   'hrsh7th/cmp-cmdline',
-    -- },
   },
 
   init = function()
@@ -48,13 +70,25 @@ return {
       }),
 
       sources = cmp.config.sources({
-        -- sources = {
+        -- { name = 'yank' },
         { name = 'nvim_lua' },
+
         -- { name = 'nvim_lsp', max_item_count = 15 },
         { name = 'nvim_lsp' },
-        { name = 'path' },
+
+        -- { name = 'path' },
+        { name = 'fuzzy_path' },
+
         { name = 'luasnip' }, -- For luasnip users.
-        { name = 'buffer', keyword_length = 4 },
+
+        -- { name = 'buffer', keyword_length = 4 },
+        { name = 'fuzzy_buffer', keyword_length = 4 },
+        {
+          name = 'rg',
+          -- Try it when you feel cmp performance is poor
+          -- keyword_length = 3
+        },
+        { name = 'buffer-lines', keyword_length = 5 },
       }),
 
       experimental = {
@@ -67,11 +101,15 @@ return {
         format = require('lspkind').cmp_format({
           mode = 'text_symbol',
           menu = {
-            buffer = '[buf]',
+            fuzzy_buffer = '[buf]',
             nvim_lsp = '[LSP]',
             nvim_lua = '[api]',
-            path = '[path]',
+            -- path = '[path]',
+            fuzzy_path = '[path]',
             luasnip = '[snip]',
+            rg = '[Rg]',
+            buffer_lines = '[bufL]',
+            -- yank = '[Yank]',
           },
         }),
       },
@@ -79,18 +117,20 @@ return {
 
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources(
-        { { name = 'path' } },
+      sources = cmp.config.sources({
+        -- { { name = 'path' } },
+        { name = 'fuzzy_path' },
         -- { { name = 'cmdline', keyword_length = 2 } }
-        { { name = 'cmdline' } }
-      ),
+        { name = 'cmdline' },
+      }),
     })
 
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
         -- { name = 'buffer', keyword_length = 2 },
-        { name = 'buffer' },
+        -- { name = 'buffer' },
+        { name = 'fuzzy_buffer' },
       },
     })
 
@@ -98,6 +138,5 @@ return {
     cmp.setup.filetype('DressingInput', {
       sources = cmp.config.sources({ { name = 'omni' } }),
     })
-
   end,
 }
