21a6a584b2af8b4f7d60b075d1ce6a3ad1a74462
isaif <saif935775@gmail.com>
2024-05-26 22:09:30 +0530

    feat(nvim): remove whole buffer textobject from mini_ai

--- dot_config/nvim/lua/plugins/mini_ai.lua
+++ dot_config/nvim/lua/plugins/mini_ai.lua
@@ -30,23 +30,25 @@ return {
         -- Whole buffer
         -- selects everything not including blank lines
         -- at start and end of buffer, aB - with them.
-        g = function(ai_type)
-          local n_lines = vim.fn.line('$')
-          local start_line, end_line = 1, n_lines
-          if ai_type == 'i' then
-            -- Skip first and last blank lines for `i` textobject
-            local first_nonblank, last_nonblank =
-              vim.fn.nextnonblank(1), vim.fn.prevnonblank(n_lines)
-            start_line = first_nonblank == 0 and 1 or first_nonblank
-            end_line = last_nonblank == 0 and n_lines or last_nonblank
-          end
-
-          local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-          return {
-            from = { line = start_line, col = 1 },
-            to = { line = end_line, col = to_col },
-          }
-        end,
+        -- Not very useful as :%y can be used to copy
+        -- :%d to delete
+        -- g = function(ai_type)
+        --   local n_lines = vim.fn.line('$')
+        --   local start_line, end_line = 1, n_lines
+        --   if ai_type == 'i' then
+        --     -- Skip first and last blank lines for `i` textobject
+        --     local first_nonblank, last_nonblank =
+        --       vim.fn.nextnonblank(1), vim.fn.prevnonblank(n_lines)
+        --     start_line = first_nonblank == 0 and 1 or first_nonblank
+        --     end_line = last_nonblank == 0 and n_lines or last_nonblank
+        --   end
+        --
+        --   local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+        --   return {
+        --     from = { line = start_line, col = 1 },
+        --     to = { line = end_line, col = to_col },
+        --   }
+        -- end,
 
         -- current line
         e = function(ai_type)
