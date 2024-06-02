03a1e2cf182ed9b5544e22626636d07115b2bb2b
isaif <saif935775@gmail.com>
2024-06-02 16:38:46 +0530

    feat(nvim): add rainbow hightlight to flash nvim
    - easier to read jump labels

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -1,11 +1,19 @@
 return {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    ---@type Flash.Config
-    ---disable it for f/F/t/T and /? search
-    opts = {
-      modes = { search = { enabled = false }, char = { enabled = false } },
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  ---disable it for f/F/t/T and /? search
+  opts = {
+    modes = { search = { enabled = false }, char = { enabled = false } },
+
+    label = {
+      rainbow = {
+        enabled = true,
+        -- number between 1 and 9
+        shade = 5,
+      },
     },
+  },
   -- stylua: ignore
     keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -14,4 +22,4 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-  }
+}
