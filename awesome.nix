a4a94fe7e52fb4b274a2c0fce8422f7fd9cf56de
isaif <saif935775@gmail.com>
2024-07-10 00:49:14 +0530

    feat(awesomewm): add awesomewm nix file

---
+++ awesome.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+let
+  awesome = pkgs.awesome.overrideAttrs (oa: {
+    version = "ad0290bc1aac3ec2391aa14784146a53ebf9d1f0";
+    src = pkgs.fetchFromGitHub {
+      owner = "awesomeWM";
+      repo = "awesome";
+      rev = "ad0290bc1aac3ec2391aa14784146a53ebf9d1f0";
+      #hash = "sha256-zCxghNGk/GsSt2+9JK8eXRySn9pHXaFhrRU3OtFrDoA=";
+      hash = "sha256-uaskBbnX8NgxrprI4UbPfb5cRqdRsJZv0YXXshfsxFU=";
+    };
+
+    patches = [ ];
+
+    postPatch = ''
+      patchShebangs tests/examples/_postprocess.lua
+    '';
+  });
+in {
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      defaultSession = "none+awesome";
+      sddm.enable = true;
+    };
+    windowManager.awesome = {
+      enable = true;
+      package = awesome;
+    };
+  };
+
+}
