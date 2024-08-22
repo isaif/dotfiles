53e9fd16c43ccdb1b55a7a6f8d6be61058993ec4
isaif <saif935775@gmail.com>
2024-08-22 16:49:35 +0530

    feat(nvim): add MiniFiles mappings for horizontal and vertical split

--- config/nvim/lua/plugins/mini_files.lua
+++ config/nvim/lua/plugins/mini_files.lua
@@ -24,6 +24,7 @@ return {
   --   },
   -- },
   config = function(_, opts)
+    -- toggle hidden files
     local MiniFiles = require('mini.files')
     local show_dotfiles = false
     local filter_show = function()
@@ -60,6 +61,37 @@ return {
         MiniFiles.refresh({ content = { filter = new_filter } })
       end,
     })
-    require('mini.files').setup(opts)
+
+    -- Open files in horizontal and vertical split
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        -- Make new window and set it as target
+        local new_target_window
+        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+          vim.cmd(direction .. ' split')
+          new_target_window = vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target_window)
+        MiniFiles.go_in({})
+        MiniFiles.close()
+      end
+
+      -- Adding `desc` will result into `show_help` entries
+      local desc = 'Split ' .. direction
+      vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        -- Tweak keys to your liking
+        map_split(buf_id, '<C-x>', 'belowright horizontal')
+        map_split(buf_id, '<C-v>', 'belowright vertical')
+      end,
+    })
+
+    MiniFiles.setup(opts)
   end,
 }
