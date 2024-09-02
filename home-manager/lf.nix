07ababac9227b323b5a54cac892107457385f9f5
isaif <saif935775@gmail.com>
2024-09-03 00:13:34 +0530

    feat(lf): Move up/down in the parent directory
    - no need to go up to change parent directory

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -8,6 +8,14 @@
           result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
           lf -remote "send $id cd \"$result\""
       }}
+
+      cmd move-parent &{{
+          dironly="setlocal \"$(dirname -- "$PWD" | sed 's/\\/\\\\/g;s/"/\\"/g')\" dironly"
+          lf -remote "send $id :updir; $dironly true; $1; $dironly false; open"
+      }}
+
+      map J move-parent down
+      map K move-parent up
     '';
   };
 
