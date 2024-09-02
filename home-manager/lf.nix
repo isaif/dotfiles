c063502e3b9f38e076985865032cc0e533612410
isaif <saif935775@gmail.com>
2024-09-03 00:29:50 +0530

    style(lf): add helpful comment

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -4,11 +4,14 @@
   programs.lf = {
     enable = true;
     extraConfig = ''
+      # zoxide integration
       cmd z %{{
           result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
           lf -remote "send $id cd \"$result\""
       }}
 
+
+      # Move up/down in the parent directory
       cmd move-parent &{{
           dironly="setlocal \"$(dirname -- "$PWD" | sed 's/\\/\\\\/g;s/"/\\"/g')\" dironly"
           lf -remote "send $id :updir; $dironly true; $1; $dironly false; open"
