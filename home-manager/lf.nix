fd5030b37fb71ef58fcc8ae6295f4f134fda8f5d
isaif <saif935775@gmail.com>
2024-09-03 00:05:53 +0530

    feat(lf): add zoxide integration

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -3,6 +3,12 @@
 {
   programs.lf = {
     enable = true;
+    extraConfig = ''
+      cmd z %{{
+          result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+          lf -remote "send $id cd \"$result\""
+      }}
+    '';
   };
 
 }
