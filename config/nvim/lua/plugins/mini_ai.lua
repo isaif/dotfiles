84d9a3ca1682ee8022145d9f3b2334332acc52b5
isaif <saif935775@gmail.com>
2024-09-12 19:13:40 +0530

    refactor(nvim): remove unnecessary code

--- config/nvim/lua/plugins/mini_ai.lua
+++ config/nvim/lua/plugins/mini_ai.lua
@@ -9,7 +9,8 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   config = function()
-    local spec_treesitter = require('mini.ai').gen_spec.treesitter
+    local gen_spec = require('mini.ai').gen_spec
+    local spec_treesitter = gen_spec.treesitter
 
     require('mini.ai').setup({
       custom_textobjects = {
@@ -18,40 +19,6 @@ return {
           a = { '@block.outer', '@conditional.outer', '@loop.outer' },
           i = { '@block.outer', '@conditional.inner', '@loop.inner' },
         }),
-
-        -- Whole buffer
-        -- my implementation ig and ag are same
-        -- g = function()
-        --   local from = { line = 1, col = 1 }
-        --   local to = {
-        --     line = vim.fn.line('$'),
-        --     col = math.max(vim.fn.getline('$'):len(), 1),
-        --   }
-        --   return { from = from, to = to }
-        -- end,
-
-        -- Whole buffer
-        -- selects everything not including blank lines
-        -- at start and end of buffer, aB - with them.
-        -- Not very useful as :%y can be used to copy
-        -- :%d to delete
-        -- g = function(ai_type)
-        --   local n_lines = vim.fn.line('$')
-        --   local start_line, end_line = 1, n_lines
-        --   if ai_type == 'i' then
-        --     -- Skip first and last blank lines for `i` textobject
-        --     local first_nonblank, last_nonblank =
-        --       vim.fn.nextnonblank(1), vim.fn.prevnonblank(n_lines)
-        --     start_line = first_nonblank == 0 and 1 or first_nonblank
-        --     end_line = last_nonblank == 0 and n_lines or last_nonblank
-        --   end
-        --
-        --   local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-        --   return {
-        --     from = { line = start_line, col = 1 },
-        --     to = { line = end_line, col = to_col },
-        --   }
-        -- end,
 
         -- current line textobject
         e = function(ai_type)
