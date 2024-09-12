6bf3c4d4ef52a051c5fe43f992c8f7b35f0da2d5
isaif <saif935775@gmail.com>
2024-09-12 19:20:39 +0530

    feat(nvim): add plugin nvim-treesitter-textsubjects

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -45,4 +45,24 @@ return {
   -- TODO: fuzzy search path in command line
   { 'Darazaki/indent-o-matic' },
   { 'sindrets/diffview.nvim' },
+  {
+    'RRethy/nvim-treesitter-textsubjects',
+    opts = {
+      textsubjects = {
+        enable = true,
+        prev_selection = ',', -- (Optional) keymap to select the previous selection
+        keymaps = {
+          ['.'] = 'textsubjects-smart',
+          [';'] = 'textsubjects-container-outer',
+          ['i;'] = {
+            'textsubjects-container-inner',
+            desc = 'Select inside containers (classes, functions, etc.)',
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  },
 }
