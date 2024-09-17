bf68e207ac17ab15bbd63a0a4ae38b69833262a6
isaif <saif935775@gmail.com>
2024-09-17 19:41:12 +0530

    feat(nvim): mini.ai add new textobjects

--- config/nvim/lua/plugins/mini_ai.lua
+++ config/nvim/lua/plugins/mini_ai.lua
@@ -14,14 +14,30 @@ return {
 
     require('mini.ai').setup({
       custom_textobjects = {
-        F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+        f = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
         o = spec_treesitter({
           a = { '@block.outer', '@conditional.outer', '@loop.outer' },
           i = { '@block.inner', '@conditional.inner', '@loop.inner' },
         }),
+        c = spec_treesitter({ a = '@class.outer', i = '@class.inner' }), -- class
+
+        u = gen_spec.function_call(), -- u for "Usage", remapped from 'f' to 'u'
+        U = gen_spec.function_call({ name_pattern = '[%w_]' }), -- without dot in function name
+
+        e = { -- Word with case
+          {
+            '%u[%l%d]+%f[^%l%d]',
+            '%f[%S][%l%d]+%f[^%l%d]',
+            '%f[%P][%l%d]+%f[^%l%d]',
+            '^[%l%d]+%f[^%l%d]',
+          },
+          '^().*()$',
+        },
+
+        d = { '%f[%d]%d+' }, -- digits
 
         -- current line textobject
-        e = function(ai_type)
+        s = function(ai_type)
           -- Get the current line
           local line = vim.fn.line('.')
 
@@ -59,6 +75,51 @@ return {
           else
             return i_region
           end
+        end,
+
+        -- Mini.ai indent text object
+        -- For "a", it will include the non-whitespace line surrounding the indent block.
+        -- "a" is line-wise, "i" is character-wise.
+        i = function(ai_type)
+          local spaces = (' '):rep(vim.o.tabstop)
+          local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+          local indents = {} ---@type {line: number, indent: number, text: string}[]
+
+          for l, line in ipairs(lines) do
+            if not line:find('^%s*$') then
+              indents[#indents + 1] = {
+                line = l,
+                indent = #line:gsub('\t', spaces):match('^%s*'),
+                text = line,
+              }
+            end
+          end
+
+          local ret = {} ---@type (Mini.ai.region | {indent: number})[]
+
+          for i = 1, #indents do
+            if i == 1 or indents[i - 1].indent < indents[i].indent then
+              local from, to = i, i
+              for j = i + 1, #indents do
+                if indents[j].indent < indents[i].indent then
+                  break
+                end
+                to = j
+              end
+              from = ai_type == 'a' and from > 1 and from - 1 or from
+              to = ai_type == 'a' and to < #indents and to + 1 or to
+              ret[#ret + 1] = {
+                indent = indents[i].indent,
+                from = {
+                  line = indents[from].line,
+                  col = ai_type == 'a' and 1 or indents[from].indent + 1,
+                },
+                to = { line = indents[to].line, col = #indents[to].text },
+              }
+            end
+          end
+
+          return ret
         end,
       },
     })
