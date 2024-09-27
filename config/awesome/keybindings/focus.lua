1154f50143e740872c8f1c2404ec2636d84f8f66
isaif <saif935775@gmail.com>
2024-09-28 00:09:12 +0530

    feat(awesomewm): change keybindings for focus next/previous client by id

--- config/awesome/keybindings/focus.lua
+++ config/awesome/keybindings/focus.lua
@@ -5,10 +5,10 @@ local modkey = variables.modkey
 
 -- Focus related keybindings
 awful.keyboard.append_global_keybindings({
-  awful.key({ modkey }, 'j', function()
+  awful.key({ modkey }, ']', function()
     awful.client.focus.byidx(1)
   end, { description = 'focus next by index', group = 'client' }),
-  awful.key({ modkey }, 'k', function()
+  awful.key({ modkey }, '[', function()
     awful.client.focus.byidx(-1)
   end, { description = 'focus previous by index', group = 'client' }),
   awful.key({ modkey }, 'Tab', function()
