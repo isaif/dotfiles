30fbd299a2df732e068a49670aad38fa5d9c0344
isaif <saif935775@gmail.com>
2024-10-09 17:53:55 +0530

    feat(nvim): update trouble config
    - modify keymaps and commands

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -1,27 +1,32 @@
 local keymaps = {
   {
     '<leader>xx',
-    '<cmd>TroubleToggle<cr>',
-    desc = 'Toggle trouble',
+    '<cmd>Trouble diagnostics toggle<cr>',
+    desc = 'Diagnostics (Trouble)',
   },
   {
     '<leader>xd',
-    '<cmd>TroubleToggle document_diagnostics<cr>',
-    desc = 'Document Diagnostics (Trouble)',
+    '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+    desc = 'Buffer Diagnostics (Trouble)',
   },
   {
-    '<leader>xw',
-    '<cmd>TroubleToggle workspace_diagnostics<cr>',
-    desc = 'Workspace Diagnostics (Trouble)',
+    '<leader>xs',
+    '<cmd>Trouble symbols toggle focus=false<cr>',
+    desc = 'Symbols (Trouble)',
   },
   {
     '<leader>xl',
-    '<cmd>TroubleToggle loclist<cr>',
+    '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+    desc = 'LSP Definitions / references / ... (Trouble)',
+  },
+  {
+    '<leader>xL',
+    '<cmd>Trouble loclist<cr>',
     desc = 'Location List (Trouble)',
   },
   {
     '<leader>xq',
-    '<cmd>TroubleToggle quickfix<cr>',
+    '<cmd>Trouble quickfix<cr>',
     desc = 'Quickfix List (Trouble)',
   },
   {
@@ -51,7 +56,7 @@ local keymaps = {
 -- better diagnostics list and others
 return {
   'folke/trouble.nvim',
-  cmd = { 'TroubleToggle', 'Trouble' },
+  cmd = { 'Trouble' },
   opts = { use_diagnostic_signs = true },
   keys = keymaps,
   dependencies = {
